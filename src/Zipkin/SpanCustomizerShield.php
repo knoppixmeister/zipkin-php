0dc3c942dffbb501d9b0e2ac9c7ea063c58517e9
José Carlos Chávez <jcchavezs@gmail.com>
2020-01-26 17:52:06 +0100

    feat: adds Tracer::inSpan and SpanCustomizer interface.

---
+++ src/Zipkin/SpanCustomizerShield.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zipkin;
+
+final class SpanCustomizerShield implements SpanCustomizer
+{
+    /**
+     * @var Span
+     */
+    private $delegate;
+
+    public function __construct(Span $span)
+    {
+        $this->delegate = $span;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setName(string $name): void
+    {
+        $this->delegate->setName($name);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function tag(string $key, string $value): void
+    {
+        $this->delegate->tag($key, $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function annotate(string $value, int $timestamp): void
+    {
+        $this->delegate->annotate($value, $timestamp);
+    }
+}
